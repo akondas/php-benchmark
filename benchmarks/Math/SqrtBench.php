8c126b3d31473d3fdd5caaf58d883d24ba62a1c6
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2019-02-08 22:18:13 +0100

    Bench sqrt function

---
+++ benchmarks/Math/SqrtBench.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpBenchmark\Math;
+
+use PhpBench\Benchmark\Metadata\Annotations\Iterations;
+use PhpBench\Benchmark\Metadata\Annotations\Revs;
+
+final class SqrtBench
+{
+    /**
+     * @Revs(1000000)
+     * @Iterations(5)
+     */
+    public function benchSqrtFunction() : void
+    {
+        sqrt(3.1415);
+    }
+
+    /**
+     * @Revs(1000000)
+     * @Iterations(5)
+     */
+    public function benchExponentiationOperator() : void
+    {
+        3.1415 ** (1 / 2);
+    }
+}
